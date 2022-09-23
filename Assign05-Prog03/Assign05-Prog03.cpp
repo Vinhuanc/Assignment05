ab803e096bbdb416a0c8bbbb4529aedca6d0f324
Vinhuanc <huann.chenn@gmail.com>
2022-09-22 20:08:00 -0500

    fourth program working

--- Assign05-Prog03/Assign05-Prog03.cpp
+++ Assign05-Prog03/Assign05-Prog03.cpp
@@ -3,48 +3,28 @@
 #include<cmath>
 using namespace std;
 
-//int main(){
-//    double pay = 0.01, pays, total;
-//    int day=1, days;
-//
-//    do{
-//    cout << "For how many days will the pay double?";
-//    cin >> days;
-//        cout << "Day" << "\t\t" << "Total Pay"<<"\n";
-//        cout << "------------"<<"\n";
-////0.02  0.04  0.08  0.16       total 1.26
-//
-//        for(day =1;day <= days; day++){
-//
-//            pays = pay*=2;
-//          //  pays = pay+=2;
-//        cout << day << "\t\t" << pays <<"\n";
-//
-//            total += pays;
-//
-//        }
-//        cout << "Total"<< "\t\t" <<total <<"\n";
-//
-//    } while(days >= 1);
-//}
-
-
-//this version of code displays $0.01 for Day1, only because I manually cout displayed the $ and Day1 code; and making the loop start on Day 2.
 int main(){
-    double pay = 0.01, pays, total;
-    int day=1, days;
+    double pay = 0.01, pays, total=0;
+    int day=0, days;
 
     do{
     cout << "For how many days will the pay double?";
     cin >> days;
     cout << "Day" << "\t\t" << "Total Pay"<<"\n";
     cout << "------------"<<"\n";
-    cout << "1" <<"\t\t" << "0.01"<<"\n";
-    for(day =2;day <= days; day++){
-    pays = pay*=2;
-    cout << day << "\t\t" << pays <<"\n";
-    total += pays;
+
+    for(day = 1; day <= days; day++){
+        cout << day << "\t\t" << pay <<"\n";
+       
+        total += pay;
+        pay *=2;
+        
+
     }
-    cout << "Total"<< "\t\t" <<(total+0.01) <<"\n";
+        
+    cout << "Total"<< "\t\t" <<total<<"\n";
+
     } while(days >= 1);
-}
+        cout << "Error. Entered days must be greater than 1.";
+};
+
