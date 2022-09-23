acc6d3859b5cdb0ae9cb7794eb7d10a02c88b0b6
Vinhuanc <102390736+Vinhuanc@users.noreply.github.com>
2022-09-22 20:15:25 -0500

    Update Assign05-Prog03.cpp

--- Assign05-Prog03/Assign05-Prog03.cpp
+++ Assign05-Prog03/Assign05-Prog03.cpp
@@ -1,4 +1,16 @@
 //Huan Chen Assignment05-Program03
+
+// Write a program that calculates how much a person would earn over a period of time if his or her salary is one penny the first day and two pennies the second day, and continues to double each day. The program should ask the user for the number of days. Display the table showing how much the salary was for each day, and then show the total pay at the end of the period. The output should be displayed in a dollar amount, not the number of pennies. Do not accept a number less than 1 for the number of days worked.
+// • Use a do-while loop for input validation
+// • Use a for loop to calculate the pay increase and display the table
+// Sample Output:
+// For how many days will the pay double? 6
+// Day       Total Pay
+// ---------------------
+// 1 $0.01 2 $0.02 3 $0.04 4 $0.08 5 $0.16 6 $0.32
+// ---------------------
+// Total     $   0.63
+// Press any key to continue . . .
 #include<iostream>
 #include<cmath>
 using namespace std;
